37ef90e5eafd6133aa21cfbfc074ebacdd16f1da
cslev <cslev@gmx.com>
2019-05-10 18:03:54 +0800

    fix

--- tools_for_measurements/send_diagonal_attack_dstport.py
+++ tools_for_measurements/send_diagonal_attack_dstport.py
@@ -14,7 +14,7 @@ while True:
     for i in DIAGONAL_DST_PORTS:
         p=((Ether(src='00:11:22:33:44:55',dst='00:11:22:33:44:66')/IP(src='10.0.0.1',dst='10.0.0.2')/UDP(sport=12345,dport=i)))
         #p.show()
-        sendp(p, iface="ns1_veth_ns")
+        sendp(p, iface="ns1_veth_ns", verbose=False)
 
 #whenever processing jumps here we delete pid file as not required anymore
 os.unlink(PID_FILE)
