e1b8a48b6e545f0c44de4d7fd478f57eaebee2fa
cslev <cslev@gmx.com>
2019-05-23 12:28:46 +0800

    Add run time argument for enabling/disabling deleting the installed flow rules when new random port filter is added

--- tools_for_measurements/add_random_port_filter.sh
+++ tools_for_measurements/add_random_port_filter.sh
@@ -6,9 +6,9 @@ function print_help {
   echo
   c_print "none" "Add random allow rule on destination port to OVS with a catch-all drop rule"
   c_print "none" "Usage:" 0
-  c_print "bold" "./add_random_port_filter.sh <ovs-br-name> <lower_range> <upper_range>"
-  c_print "none" "Example for random well-known ports"
-  c_print "none" "./add_random_port_filter.sh ovsbr 1 1024"
+  c_print "bold" "./add_random_port_filter.sh <ovs-br-name> <lower_range> <upper_range> <REMOVE_OLD?=0/1>"
+  c_print "none" "Example for adding a random port filter in the well-known ports' range without removing old flows"
+  c_print "none" "./add_random_port_filter.sh ovsbr 1 1024 0"
   echo
   exit -1
 }
@@ -23,6 +23,7 @@ fi
 DBR=$1
 R1=$2
 R2=$3
+REMOVE_OLD=$4
 
 FLOW_RULE_FILE="malicious_acl_diagonal_attack.flows"
 RPL="REPLACE"
@@ -31,16 +32,15 @@ RPL="REPLACE"
 DIFF=$(($R2-$R1+1))
 R=$(($(($RANDOM%$DIFF))+$R1))
 
-c_print "yellow" "Removing existing flows from the table" 0
-sudo ovs-ofctl del-flows $DBR
-c_print "green" "[DONE]"
-
+if [ $REMOVE_OLD -eq 1 ]
+then
+  c_print "yellow" "Removing existing flows from the table" 0
+  sudo ovs-ofctl del-flows $DBR
+  c_print "green" "[DONE]"
+fi
 
 c_print "green" "Chosen random port is ${R}"
 c_print "none"  "Adding flow rules accordingly..." 0
 sed  "s/${RPL}/${R}/" $FLOW_RULE_FILE > "${FLOW_RULE_FILE}.tmp"
 sudo ovs-ofctl add-flows $DBR "${FLOW_RULE_FILE}.tmp"
 c_print "green" "[DONE]"
-
-
-
