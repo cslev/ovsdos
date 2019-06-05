88c309aa1d36e56020a804d7eada43d18fafd304
cslev <cslev@gmx.com>
2019-06-05 09:57:44 +0800

    Update according to the new random header generator script

--- tools_for_measurements/random_srcip_dstport_attack_measurement.sh
+++ tools_for_measurements/random_srcip_dstport_attack_measurement.sh
@@ -3,7 +3,7 @@ source colorized_output.sh
 
 function print_help {
   echo
-  c_print "none" "Add random allow rule on destination port and another one on source IP address to OVS with a catch-all drop rule and send n*n random packets from already generated PCAP files (n=32,45,71,100,142). Do the whole thing 100 times!"
+  c_print "none" "Add random allow rule on destination port and another one on source IP address to OVS with a catch-all drop rule and send n random packets generated online (n=,17,34,68,85,170,850,1700,2500, 5000, 7500, 10000, 25000, 50000). Do the whole thing 100 times!"
   c_print "none" "Usage:" 0
   c_print "bold" "./random_srcip_dstport_attack_measurement.sh"
   c_print "none" "Example:"
@@ -13,27 +13,32 @@ function print_help {
 }
 
 ITERATION=100
+R1=1
+R2=65535
 
-for i in 20000 50000 750000 100000
+for i in 17 34 68 85 170 850 1700 2500  5000 7500 10000 25000 50000
 do
   echo "i, megaflow_entries" > "random_srcip_dstport_attack_${i}.csv"
 
   for iter in $(seq 1 $ITERATION)
   do
     c_print "blue" "[MAIN THREAD]\t Generate random packet sequence"
-    python full_random_attack_dport_srcip.py -n $i > ../pcap_generator/random_srcip_dport/SIP_DP_${i}_${iter}.csv
+    python full_random_header_generator.py -n $i -ab > ../pcap_generator/random_srcip_dport/SIP_DP_${i}_${iter}.csv
     c_print "blue" "[MAIN THREAD]\t Generate pcap file from packet sequence"
     python ../pcap_generator/pcap_generator_from_csv.py -i ../pcap_generator/random_srcip_dport/SIP_DP_${i}_${iter}.csv --dst_ip 10.0.0.2 -o ../pcap_generator/random_srcip_dport/SIP_DP_${i}_${iter}
     c_print "green" "[DONE]"
 
     c_print "blue" "[MAIN THREAD]\t Add flow rule with a random port number"
-    ./add_random_port_filter.sh ovsbr 1 65535 1
+    sudo ovs-ofctl del-flows ovsbr
+    DIFF=$(($R2-$R1+1))
+    R=$(($(($RANDOM%$DIFF))+$R1))
+    sudo ovs-ofctl add-flow ovsbr "table=0,priority=1000,udp,in_port=3,nw_dst=10.0.0.2,tp_dst=${R},actions=output:2"
     sleep 1
     c_print "green" "[DONE]"
 
     c_print "blue" "[MAIN THREAD]\t Add flow rule with a random source IP address" 0
     RANDOM_IP=$(echo $((RANDOM%256)).$((RANDOM%256)).$((RANDOM%256)).$((RANDOM%256)))
-    sudo ovs-ofctl add-flow ovsbr "table=0,priority=1000,udp,in_port=1,nw_dst=10.0.0.2,nw_src=${RANDOM_IP},actions=output:2"
+    sudo ovs-ofctl add-flow ovsbr "table=0,priority=1000,udp,in_port=3,nw_dst=10.0.0.2,nw_src=${RANDOM_IP},actions=output:2"
     sleep 1
     c_print "green" "[DONE]"
 
