b79568f65c69817ccc25b81451f1b41abf0820c5
cslev <cs.lev@gmx.com>
2020-02-20 12:25:38 +0100

    update start iperf server to delete prvious log file, and being able to monitor if parallel iperf sessions are being carried out

--- tools_for_measurements/start_iperf_server.sh
+++ tools_for_measurements/start_iperf_server.sh
@@ -11,6 +11,8 @@ source colorized_output.sh
 # $3: no_newline - if nothing is provided newline will be printed at the end
 #				 - anything provided, NO newline is indicated
 
+rm iperf.out
+
 c_print yellow "Starting iperf in background mode" 1
 iperf3 -s -i 1 -f g --logfile iperf.out &
 c_print green "[DONE]"
@@ -19,10 +21,25 @@ c_print green "[DONE]"
 
 while true
 do
+  a=$(cat iperf.out|grep -i sum)
+ # echo $a
+  retval=$(echo $?)
+  if [ $retval -ne 0 ]
+  then
+    value=$(tail -n1 iperf.out |grep "Gbits/sec"|awk '{print $7}')
+  else
+    value=$(cat iperf.out |grep "\- \-" -B +1| grep "SUM"|tail -n1|awk '{print $6}')
+  fi
+  echo "---"
+  echo $value
+  echo "---"
+
   curl -i -XPOST 'http://172.17.0.1:8086/write?db=iperf' \
-  --data-binary "throughput,received=Gbit value=$(tail -n1 iperf.out |grep "Gbits/sec"|awk '{print $7}')"
+    --data-binary "throughput,received=Gbit value=${value}"
 
   sleep 1s
 done
+
+
 
 
