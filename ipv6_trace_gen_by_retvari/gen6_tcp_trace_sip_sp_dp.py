cb8d72317fcfa3ed72f6882ea4572e1dfe66c7cf
cslev <cs.lev@gmx.com>
2020-04-29 12:35:45 +0800

    fix ipv6 SIP_SP_DP generation by adding the holes into the traces as well

--- ipv6_trace_gen_by_retvari/gen6_tcp_trace_sip_sp_dp.py
+++ ipv6_trace_gen_by_retvari/gen6_tcp_trace_sip_sp_dp.py
@@ -41,27 +41,47 @@ src_ip_list = [ord(x) for x in list(struct.unpack("!16c", src_ip.packed))]
 
 # pprint.pprint(src_ip_list)
 
+spoof_dp_list = list()
+spoof_sip_list = list()
+spoof_sp_list = list()
+
 for i in range(16):
     n = list(bin(src_ip_list[i])[2:].zfill(8))
-    for j in range(8):
-        s = src_ip_list[:]
-        s[i] = s[i] - (1 << (7-j)) if int(n[j]) == 1 else s[i] + (1 << (7-j))
-        spoof_sip = ipaddress.IPv6Address(bytes(s))
-
+    for j in range(9):
+        if j==8:
+            spoof_sip=src_ip
+        else:
+            if int(n[j]) == 1:
+                s[i] = s[i] - (1 << (7-j))
+            else:
+                s[i] = s[i] + (1 << (7-j))
+            s = src_ip_list[:]
+            spoof_sip = ipaddress.IPv6Address(bytes(s))
+        spoof_sip_list.append(spoof_sip)
         # pprint.pprint(str(spoof_sip))
         # pprint.pprint(args.dst_ip)
-        
+
         # src port
         m = list(bin(src_tcp)[2:].zfill(16))
-        for k in range(16):
-            spoof_sp = src_tcp - (1 << (15-k)) if int(m[k]) == 1 else \
-                       src_tcp + (1 << (15-k))
-
+        for k in range(17):
+            if k==16:
+                spoof_sp=src_tcp
+            else:
+                if int(m[k]) == 1:
+                    spoof_sp = src_tcp - (1 << (15-k))
+                else:
+                    spoof_sp = src_tcp + (1 << (15-k))
+            spoof_sp_list.append(spoof_sp)
             # dst port
             n = list(bin(dst_tcp)[2:].zfill(16))
-            for l in range(16):
-                spoof_dp = dst_tcp - (1 << (15-l)) if int(n[l]) == 1 else \
-                           dst_tcp + (1 << (15-l))
+            for l in range(17):
+                if l==16:
+                    spoof_dp=dst_tcp
+                else:
+                    if int(n[l]) == 1:
+                        spoof_dp = dst_tcp - (1 << (15-l))
+                    else:
+                        spoof_dp = dst_tcp + (1 << (15-l))
 
                 p.append(Ether(src=args.src_mac, dst=args.dst_mac)/ \
                          IPv6(src=str(spoof_sip), dst=args.dst_ip)/ \
@@ -70,7 +90,7 @@ for i in range(16):
                      # p.append(IPv6(src=str(spoof_sip), dst=args.dst_ip)/ \
                      #          TCP(sport=20, dport=int(spoof_dp))/ \
                      #          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
-            
+
 sys.stderr.write("Number of packets generated: %d\n" % len(p))
 
 if args.shuffle == True:
